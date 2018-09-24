b2a0c3091c6bd00d77e00f0efa49478abaf43b3c
Michael Brunton-Spall <michael@brunton-spall.co.uk>
2018-09-24 09:53:11 +0100

    Add markdown parser for notes

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import logging
 from flask import Flask
 from models import Newsletter, Link, Settings
 from flask import request, render_template, redirect, url_for
+from flaskext.markdown import Markdown
 import requests_toolbelt.adapters.appengine
 
 from handlers.admin import admin
@@ -15,8 +16,9 @@ from handlers.fetch import fetch
 requests_toolbelt.adapters.appengine.monkeypatch()
 
 app = Flask(__name__)
+Markdown(app)
 
-app.register_blueprint(admin)
+app.register_blueprint(admin, url_prefix='/admin')
 app.register_blueprint(newsletter, url_prefix='/newsletter')
 app.register_blueprint(links, url_prefix='/link')
 app.register_blueprint(fetch, url_prefix='/fetch')
