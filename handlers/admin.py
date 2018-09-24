08dd1de49a115d03ae7523ba1c4ab6e8cefe4d97
Michael Brunton-Spall <michael@brunton-spall.co.uk>
2018-09-24 09:52:24 +0100

    Add an import data endpoint

--- handlers/admin.py
+++ handlers/admin.py
@@ -4,3 +4,23 @@ from flask import Blueprint, redirect
 from datetime import date
 
 admin = Blueprint('admin', __name__)
+
+
+@admin.route('/testdata')
+def testdata():
+    link = Link(url='http://www.iso27001security.com/html/27000.html',
+                type=Link.DRAFT,
+                title='ISMS/ISO27k overview section',
+                quote='I wish the committee would replace “information security risk” throughout ISO27k with the simpler and more appropriate term “information risk”.',
+                note='A note').put()
+    link = Link(url='https://www.riskiq.com/blog/labs/magecart-british-airways-breach/',
+                type=Link.DRAFT,
+                title='Inside the Magecart Breach of British Airways: How 22 Lines of Code Claimed 380,000 Victims',
+                quote='On September 6th, British Airways announced it had suffered a breach resulting in the theft of customer data. In interviews with the BBC, the company noted that around 380,000 customers could have been affected and that the stolen information included personal and payment information but not passport information. ',
+                note='A note').put()
+
+    newsletter = Newsletter(number='1').put()
+    for link in Link.queued():
+        link.newsletter = newsletter
+        link.put()
+    return redirect('/')
