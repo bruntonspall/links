f2c184c6117b0b5cbbad7973fc18721be54d967d
Michael Brunton-Spall <michael@brunton-spall.co.uk>
2022-01-27 18:25:19 +0000

    Refactor CSS out into classes a bit more sensibly

--- app/main.py
+++ app/main.py
@@ -237,7 +237,7 @@ if __name__ == '__main__':
     def login_test_user():
         user = User.create("testuser", "test@test.com", "Test User", "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80")
         login_user(user)
-    app.config['LOGIN_DISABLED'] = True
+    # app.config['LOGIN_DISABLED'] = True
     import os
     if os.path.exists("export.json"):
         import_from_file("export.json")
