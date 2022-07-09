5c0603753674a3549d5c30104bffa1183602d1dd
Michael Brunton-Spall <michael@brunton-spall.co.uk>
2022-07-09 21:32:04 +0100

    Move admin features over to repos

--- app/handlers/admin.py
+++ app/handlers/admin.py
@@ -1,17 +1,13 @@
 # -*- coding: UTF-8 -*-
+from datetime import date, datetime
 from models.newsletter import Newsletter
 from models.link import Link
-from models.database import Database
-from services import links_service, newsletter_service
-from repositories import links_repo
-from flask import Blueprint, redirect, request
-from flask_login import login_required
+from repositories import links_repo, newsletter_repo
+from flask import Blueprint, request
 import json
 import logging 
-from dateutil import parser
 
 admin = Blueprint('admin', __name__)
-from datetime import date, datetime
 
 
 def json_serial(obj):
@@ -30,18 +26,19 @@ def export():
         'readinglist': [],
         'drafts': []
     }
-    for newsletter in Newsletter.list():
+    for newsletter in newsletter_repo.list():
         news = newsletter.to_dict()
         news['links'] = [link.to_dict() for link in links_repo.by_newsletter(newsletter.key)]
         data['newsletters'].append(news)
-    for link in Link.drafts():
+    for link in links_repo.drafts():
         data['drafts'].append(link.to_dict())
-    for link in Link.queued():
+    for link in links_repo.queued():
         data['queue'].append(link.to_dict())
-    for link in Link.toread():
+    for link in links_repo.toread():
         data['readinglist'].append(link.to_dict())
 
     return json.dumps(data, indent=4, default=json_serial)
+
 
 @admin.route('/import', methods=['POST'])
 def imp():
@@ -56,36 +53,19 @@ def imp():
         n = Newsletter.from_dict(nl)
         n.body = nl['intro']
         n.slugify()
-        n.save()
+        newsletter_repo.save(n)
         key = n.key()
         logging.info(u"Create newsletter {} - {}".format(n.number, n.title))
-        for l in nl['links']:
-            link = Link.from_dict(l)
+        for link_d in nl['links']:
+            link = Link.from_dict(link_d)
             link.newsletter = key
-            
-            link.save()
+            links_repo.save(link)
             logging.info(u"Create link {}".format(link.title))
-    for l in data['queue']:
-        link = Link.from_dict(l)
-        link.save()
-    for l in data['drafts']:
-        link = Link.from_dict(l)
-        link.save()
-    for l in data['readinglist']:
-        link = Link.from_dict(l)
-        link.save()
+    for link in data['queue']:
+        links_repo.save(Link.from_dict(link))
+    for link in data['drafts']:
+        links_repo.save(Link.from_dict(link))
+    for link in data['readinglist']:
+        links_repo.sve(Link.from_dict(link))
 
-    return "OK"
-
-
-@admin.route('/migrate', methods=['POST'])
-def migrate():
-    for newsletter in Database.db.collection(Newsletter.collection).stream():
-        d = newsletter.to_dict()
-        if isinstance(d['stored'], str):
-            stored = datetime.fromisoformat(d['stored'])
-            Database.db.collection(Newsletter.collection).document(newsletter.id).update({'stored': stored})
-        if isinstance(d['sentdate'], str):
-            stored = datetime.fromisoformat(d['sentdate'])
-            Database.db.collection(Newsletter.collection).document(newsletter.id).update({'sentdate': stored})
     return "OK"
